255927f10bde955d1ee0c8ed1630c8f716db3aee
vidalgmartin <vidalgmartin@hotmail.com>
2024-03-12 10:56:27 -0400

    Home page styling

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -15,16 +15,13 @@ export default function Home() {
         <div className="home-page">
 
             <section className="intro-section">
-                
                 <div className="intro-content">
                     <h2>Lorem ipsum dolor. ??</h2>
                     <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
                     <div>
-                        <button onClick={scrollToCollections}>Explore collections</button>
-                        <Link to="/products/all">
-                            <button>
-                                Shop All
-                            </button>
+                        <a className="intro-button" onClick={scrollToCollections}>Explore collections</a>
+                        <Link className="intro-button"to="/products/all">
+                            Shop All
                         </Link>
                     </div>
                 </div>
@@ -32,23 +29,29 @@ export default function Home() {
 
             <section className="products-section" ref={targetRef} >
                 <div className="products-content">
-                    <h1>Shop by category</h1>
+                    <div className="products-headline">
+                        <h3>Shop by category</h3>
+                    </div>
                     <div className="products-panels">
-                        <Link to="/products/cat-suits">
+                        <Link className="panel" to="/products/cat-suits">
                             Cat suits
                         </Link>
-                        <Link to="/products/dog-suits">
+                        <Link className="panel" to="/products/dog-suits">
                             Dog suits
                         </Link>
-                        <Link to="/products/accessories">
+                        <Link className="panel" to="/products/accessories">
                             Acessories
                         </Link>
                     </div>
                 </div>
             </section>
 
-            <footer className="footer-links">
-                <a>Github</a> <a>LinkedIn</a> by Martin Vidal Garibay
+            <footer>
+                <div className="footer-links">
+                    <a>Github</a>
+                    <a>LinkedIn</a>
+                    <a>Portfolio</a>
+                </div>
             </footer>
 
         </div>
