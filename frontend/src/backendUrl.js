adb5ad42848e7d5ee813fb72be7e1ba8f64c5560
vidalgmartin <vidalgmartin@hotmail.com>
2024-04-24 11:46:43 -0400

    testing backend url

--- frontend/src/backendUrl.js
+++ frontend/src/backendUrl.js
@@ -1,5 +1,7 @@
-export const backendUrl = 'https://mern-todo-app-rbk4.onrender.com'
+export const backendUrl = 'https://pet-suits-ecommerce.onrender.com'
 
 // https://mern-todo-app-rbk4.onrender.com
 
 // http://localhost:5002
+
+// https://pet-suits-ecommerce.onrender.com
